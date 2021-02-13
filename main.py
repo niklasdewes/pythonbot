4848d3c44098b2e09edbe0c29015013b9f59d54c
Niklas Dewes <nt.dewes@gmail.com>
2021-02-13 16:18:07 +0100

    delete commands.py & roulette.py |  change all code from this files into main.py

--- main.py
+++ main.py
@@ -1,22 +1,59 @@
 import discord
-from commands import cmd
-from roulette import rou
+import random
 
 class MyClient(discord.Client):
     #Login
     async def on_ready(self):
-        print("I've been login.")
+        print("""Der Bot wurde erfolgreich gestartet.
+---------------------
+Version: 0.1
+Auhtor: Niklas Dewes""")
 
-    cmd()
-    rou()
+    async def on_message(self, message):
+        if message.author == MyClient.user:
+            return  
+        if message.content == "!help":
+            await message.channel.send("""Python Discord Bot von Niklas Dewes
+---------------------
+!roulette <BID> - Spiele Roulette. Als BID kommt 'red', 'black' & 'number(0-36)' in Frage.""")
 
+        if message.author == MyClient.user:
+            return
+
+        if message.content.startswith("!rl"):
+            bid = message.content.split(' ')[1]
+            bid_param = -3
+            if bid.lower() == "black":
+                bid_param = -1
+            elif bid.lower() == "red":
+                bid_param = -2
+            else:
+                try:
+                    bid_param = int(bid)
+                except:
+                    bid_param = -3
+            if bid_param == -3:
+                await message.channel.send('Ungültige Eingabe')
+                return
+            result = random.randint(0,36)
+            print(result)
+            if bid_param == -1:
+                won = result%2 == 0 and not result == 0
+            elif bid_param == -2:
+                won = result%2 == 1
+            else:
+                won = result == bid_param
+            if won:
+                await message.channel.send('$$$ Du hast gewonnen $$$')
+            else:
+                await message.channel.send('Leider verloren...')
 def main():
     MyClient()
 
-client = MyClient()
-k = open('key.txt')
-mykey = k.readline()
-client.run(mykey)
+    client = MyClient()
+    k = open('key.txt')
+    mykey = k.readline()
+    client.run(mykey)
 
 if __name__ == "__main__":
     main()
