7412051c466c7597e6b9c8853b9c0e2e89a76f83
Niklas Dewes <nt.dewes@gmail.com>
2021-02-14 12:00:49 +0100

    Initial commit

--- main.py
+++ main.py
@@ -1,59 +1,90 @@
-import discord
 import random
+import discord
+from discord import Member, Guild
+import asyncio
 
-class MyClient(discord.Client):
-    #Login
-    async def on_ready(self):
-        print("""Der Bot wurde erfolgreich gestartet.
----------------------
-Version: 0.1
-Auhtor: Niklas Dewes""")
+client = discord.Client(intents=discord.Intents.all())
 
-    async def on_message(self, message):
-        if message.author == MyClient.user:
-            return  
-        if message.content == "!help":
-            await message.channel.send("""Python Discord Bot von Niklas Dewes
----------------------
-!roulette <BID> - Spiele Roulette. Als BID kommt 'red', 'black' & 'number(0-36)' in Frage.""")
+antworten = ['Ja', 'Nein', 'Vielleicht', 'Wahrscheinlich', 'Sieht so aus', 'Sehr wahrscheinlich',
+            'Sehr unwahrscheinlich']
 
-        if message.author == MyClient.user:
-            return
+@client.event
+async def on_ready():
+    print(f"Eingeloggt als: {client.user.name}\r\n",
+          "Version: 0.1")
+    client.loop.create_task(status_task())
 
-        if message.content.startswith("!rl"):
-            bid = message.content.split(' ')[1]
-            bid_param = -3
-            if bid.lower() == "black":
-                bid_param = -1
-            elif bid.lower() == "red":
-                bid_param = -2
-            else:
-                try:
-                    bid_param = int(bid)
-                except:
-                    bid_param = -3
-            if bid_param == -3:
-                await message.channel.send('Ungültige Eingabe')
-                return
-            result = random.randint(0,36)
-            print(result)
-            if bid_param == -1:
-                won = result%2 == 0 and not result == 0
-            elif bid_param == -2:
-                won = result%2 == 1
-            else:
-                won = result == bid_param
-            if won:
-                await message.channel.send('$$$ Du hast gewonnen $$$')
-            else:
-                await message.channel.send('Leider verloren...')
-def main():
-    MyClient()
+async def status_task():
+    colors = [discord.Colour.red(), discord.Colour.orange(), discord.Colour.gold(), discord.Colour.green(), discord.Colour.blue(), discord.Colour.purple()]
+    while True:
+        await client.change_presence(activity=discord.Game("Bot ertellt von Niklas Dewes"), status=discord.Status.online)
+        await asyncio.sleep(5)
+        await client.change_presence(activity=discord.Game("Nur Augen für Amy!"), status=discord.Status.online)
+        await asyncio.sleep(5)
+        await client.change_presence(activity=discord.Game("!help für Hilfe"), status=discord.Status.online)
+        await asyncio.sleep(5)
+        guild: Guild = client.get_guild(782334678392111175)
+        if guild:
+            role = guild.get_role(810282842030342154)
+            if role:
+                if role.position < guild.get_member(client.user.id).top_role.position:
+                    await role.edit(colour=random.choice(colors))
 
-    client = MyClient()
-    k = open('key.txt')
-    mykey = k.readline()
-    client.run(mykey)
 
-if __name__ == "__main__":
-    main()
+def is_not_pinned(mess):
+    return not mess.pinned
+
+@client.event
+async def on_message(message):
+    if message.author.bot:
+        return
+    if "!help" in message.content:
+        await message.channel.send("**Python Discord Bot von Niklas Dewes**\r\n"
+                                    "---------------------\n"
+                                    "**!help** - Zum aufrufen aller Befehle.\n"
+                                    "**!userinfo** - Zeigt dir die Userinfo von Personen an.\n"
+                                    "**!clear** - Damit kannst du Nachrichten löschen, brauchst aber dafür die Berechtigungen.\n"
+                                    "**!8b <Frage>** - Wahrheitskugel. Stell eine Frage und die Frage wird dir beantwortet.\n")
+    if message.content.startswith('!userinfo'):
+        args = message.content.split(' ')
+        if len(args) == 2:
+            member: Member = discord.utils.find(lambda m: args[1] in m.name, message.guild.members)
+            if member:
+                embed = discord.Embed(title='Userinfo für {}'.format(member.name),
+                                      description='Dies ist eine Userinfo für den User {}'.format(member.mention),
+                                      color=0x22a7f0)
+                embed.add_field(name='Server beigetreten', value=member.joined_at.strftime('%d/%m/%Y, %H:%M:%S'),
+                                inline=True)
+                embed.add_field(name='Discord beigetreten', value=member.created_at.strftime('%d/%m/%Y, %H:%M:%S'),
+                                inline=True)
+                rollen = ''
+                for role in member.roles:
+                    if not role.is_default():
+                        rollen += '{} \r\n'.format(role.mention)
+                if rollen:
+                    embed.add_field(name='Rollen', value=rollen, inline=True)
+                embed.set_thumbnail(url=member.avatar_url)
+                embed.set_footer(text='Ich bin ein EmbedFooter.')
+                mess = await message.channel.send(embed=embed)
+    if message.content.startswith('!clear'):
+        if message.author.permissions_in(message.channel).manage_messages:
+            args = message.content.split(' ')
+            if len(args) == 2:
+                if args[1].isdigit():
+                    count = int(args[1]) + 1
+                    deleted = await message.channel.purge(limit=count, check=is_not_pinned)
+    if message.content.startswith('!8b'):
+        args = message.content.split(' ')
+        if len(args) >= 2:
+            frage = ' '.join(args[1:])
+            mess = await message.channel.send('Ich versuche deine Frage `{0}` zu beantworten.'.format(frage))
+            await asyncio.sleep(2)
+            await mess.edit(content='Ich kontaktiere das Orakel...')
+            await asyncio.sleep(2)
+            await mess.edit(content='Deine Antwort zur Frage `{0}` lautet: `{1}`'
+                            .format(frage, random.choice(antworten)))
+
+
+k = open('key.txt')
+mykey = k.readline()
+client.run(mykey)
